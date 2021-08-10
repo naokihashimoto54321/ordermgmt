5779c3469869dcb9dd1c98756beba9a664b37f50
naoki.hashimoto <naoki.hashimoto54321@gmail.com>
2021-08-10 19:51:40 +0900

    テスト

---
+++ spring boot/ordermgmt/src/main/java/jp/cnt/ordermgmt/OrdermgmtApplication.java
@@ -0,0 +1,13 @@
+package jp.cnt.ordermgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermgmtApplication.class, args);
+	}
+
+}
---
+++ spring boot/ordermgmt/src/test/java/jp/cnt/ordermgmt/OrdermgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.cnt.ordermgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
